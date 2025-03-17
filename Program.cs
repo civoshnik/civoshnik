72df02a694970e990efafbe5ca3f6858df26553d
civoshnik <kaka_alex@inbox.ru>
2025-03-17 09:11:16 +0300

    Initial commit: добавлены файлы проекта

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt1
+{
+    public class Animal
+    {
+        public string Name;
+        public string Breed;
+        public int Age;
+        public Animal(string name, string breed, int age) { 
+            Name = name;
+            Breed = breed;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
