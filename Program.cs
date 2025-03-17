5b9d665f8860c4e069b026ef7520a3ff93f06231
civoshnik <kaka_alex@inbox.ru>
2025-03-17 09:15:22 +0300

    Реализован метод Main в Program.cs

--- Program.cs
+++ Program.cs
@@ -6,17 +6,6 @@ using System.Threading.Tasks;
 
 namespace Prakt1
 {
-    public class Animal
-    {
-        public string Name;
-        public string Breed;
-        public int Age;
-        public Animal(string name, string breed, int age) { 
-            Name = name;
-            Breed = breed;
-            Age = age;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
